7fe5961dd0a04f3ef9d8b3002a35884fc2667808
OctaconDeveloper <kaakadev@gmail.com>
2020-07-25 00:42:15 +0100

    add ambulance user flow

--- pages/ambulance/verify/index.vue
+++ pages/ambulance/verify/index.vue
@@ -1,19 +1,19 @@
 <template>
 <!-- BEGIN: Content-->
-    <div class="app-content content justify-content-center" style="margin-left:0;">
+    <div class="app-content content">
         <div class="content-overlay"></div>
         <div class="header-navbar-shadow"></div>
         <div class="content-wrapper">
             <div class="content-header row">
             </div>
             <div class="content-body">
-                <section class="row flexbox-container justify-content-center">  
+                <section class="row flexbox-container">
                     <div class="col-xl-8 col-11 d-flex justify-content-center">
                         <div class="card rounded-0 mb-0">
                             <div class="row m-0">
                                 <div class="col-lg-12" style="display:flex;justify-content: center;">
-                                    <img src="~assets/img/logo/drcallaway-logo.png" class="mb-2 mt-1" width="100px" height="90px">
-                                    <!-- <h2 class="brand-text mb-2 mt-1" style="align-self: center;">Drcallaway</h2> -->
+                                    <img src="~assets/img/logo/drcallaway-logo.png" class="mb-2 mt-1" width="45px" height="40px">
+                                    <h2 class="brand-text mb-2 mt-1" style="align-self: center;">Drcallaway</h2>
                                 </div>
                                 <div class="col-lg-12 col-12 p-0" >
                                     <div class="card rounded-0 mb-0 px-2">
@@ -23,19 +23,17 @@
                                             </div>
                                         </div> -->
                                         <div class="card-title">
-                                            <h3 class="text-center"><u>Drcallaway</u></h3>
+                                            <h3 class="text-center"><u>Verify Your Account</u></h3>
                                         </div>
-                                        <p class=" text-center">Welcome to a world where we literally treat health as wealth.</p>
-                                        <ion-icon name="chevron-down-outline" style="align-self:center;text-alifgn:center"></ion-icon>
+                                        <p class=" text-center">Kindly verify your email address by
+                                        clicking the link sent to your mailbox.</p>
                                         <div class="card-content">
                                             <div class="card-body pt-1 d-flex justify-content-center">
-                                                <nuxt-link to="/patients/signup" style="text-align: center;">Patient | </nuxt-link>
-                                                <nuxt-link to="/doctor/signup" style="text-align: center;"> Doctor | </nuxt-link>
-                                                <nuxt-link to="/ambulance/signup" style="text-align: center;"> Ambulance </nuxt-link>
-                                            </div> 
+                                                <nuxt-link to="/ambulance/dashboard" style="text-align: center;">Click here to continue</nuxt-link>
+                                            </div>
                                         </div>
                                         <div class="login-footer">
-                                            
+
                                         </div>
                                     </div>
                                 </div>
@@ -51,6 +49,6 @@
     <!-- END: Content-->
     <script>
 export default {
-  name: 'Home'
+  name: 'Verify'
 }
 </script>
