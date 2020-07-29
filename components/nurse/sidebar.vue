559ad24b206eade89fbb9f06a751af00499c19f8
Oluwatosin Ikudayisi <oluwatosin.ikudayisi@cashdrive.co>
2020-07-29 19:17:45 +0100

    nurse dashboard

---
+++ components/nurse/sidebar.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <div
+      class="main-menu menu-fixed menu-light menu-accordion"
+      data-scroll-to-active="true"
+    >
+      <div class="navbar-header">
+        <ul class="nav navbar-nav flex-row">
+          <li class="nav-item mr-auto">
+            <nuxt-link class="navbar-brand" to="#">
+              <div class="brand-logo"></div>
+              <h2 class="brand-text mb-0">Drcallaway</h2>
+            </nuxt-link>
+          </li>
+          <li class="nav-item nav-toggle">
+            <nuxt-link
+              to="#"
+              class="nav-link modern-nav-toggle pr-0"
+              data-toggle="collapse"
+              ><i
+                class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"
+              ></i
+              ><i
+                class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary"
+                data-ticon="icon-disc"
+              ></i
+            ></nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div class="shadow-bottom"></div>
+      <div class="main-menu-content">
+        <ul
+          id="main-menu-navigation"
+          class="navigation navigation-main"
+          data-menu="menu-navigation"
+        >
+          <li class="nav-item active">
+            <nuxt-link to="/nurse/dashboard"
+              ><i class="feather icon-home"></i
+              ><span class="menu-title" data-i18n="Dashboard">Dashboard</span
+              >
+              <!-- <span class="badge badge badge-warning badge-pill float-right"
+                >2</span
+              > -->
+              </nuxt-link
+            >
+            
+          </li>
+          <hr />
+          <li class=" nav-item">
+            <nuxt-link to="/nurse/profile"
+              ><ion-icon name="person-outline" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title">Profile</span>
+            </nuxt-link>
+          </li>
+
+          <li class=" nav-item">
+            <nuxt-link to="/nurse/bookings"
+              ><ion-icon name="calendar-outline"  color="dark" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title">My Bookings</span>
+            </nuxt-link>
+          </li>
+          <li class=" nav-item ">
+            <nuxt-link to="/nurse/message"
+              ><ion-icon name="mail-unread-outline"  color="dark" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title">Messages</span>
+            </nuxt-link>
+          </li>
+          <li class=" nav-item ">
+            <nuxt-link to="patients"
+              ><ion-icon name="person-add-outline" color="dark" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title">My Patients</span>
+            </nuxt-link>
+          </li>
+          <li class=" nav-item">
+            <nuxt-link to="/nurse/reviews"
+              ><ion-icon name="calendar-outline" color="dark" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title"> Reviews</span>
+            </nuxt-link>
+          </li>
+        </ul>
+        <hr />
+        <ul
+          id="main-menu-navigation"
+          class="navigation other-link navigation-main"
+          data-menu="menu-navigation"
+        >
+          <li class=" nav-item">
+            <nuxt-link to="/nurse/help"
+              ><ion-icon name="information-circle-outline" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title">Help Desk</span>
+            </nuxt-link>
+          </li>
+          <li class=" nav-item">
+            <nuxt-link to="/nurse/changepassword"
+              ><ion-icon name="settings-outline" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title">Change Password</span>
+            </nuxt-link>
+          </li>
+          <li class=" nav-item">
+            <nuxt-link to="#"
+              ><ion-icon name="exit-outline" style="margin-right:1em;"></ion-icon>
+              <span class="menu-title" data-i18n="Raise Support"
+                >Logout</span
+              ></nuxt-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Sidebar"
+}
+</script>
+<style scoped>
+.other-link{
+    position:fixed;
+    bottom:0;
+    left:0;
+}
+</style>
---
+++ pages/nurse/reviews/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <body
+      class="vertical-layout vertical-menu-modern 2-columns  navbar-floating footer-static  "
+      data-open="click"
+      data-menu="vertical-menu-modern"
+      data-col="2-columns"
+    >
+      <Header></Header>
+      <Sidebar></Sidebar>
+
+      <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+          <div class="content-header row justify-content-center">
+            <div class="content-header-left col-md-5 col-12 mb-2">
+              <div class="row breadcrumbs-top">
+                <div class="col-12">
+                  <!-- <h4 class="content-header-title float-left mb-0">Patients List</h4> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="content-body">
+            <section class="page-users-view">
+              <div class="row justify-content-center">
+                <!-- account start -->
+                <div class="col-12 col-md-5 col-lg-5">
+                  <div class="card justify-content-center">
+                    <div class="card-header">
+                      <div class="card-title">Reviews</div>
+                      <hr />
+                    </div>
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                          <ul class="list-group list-group-flush">
+                            <li
+                              class="list-group-item d-flex justify-content-between align-items-center"
+                            >
+                            <nuxt-link
+                                class="d-flex justify-content-between"
+                                to="#"
+                                data-toggle="dropdown"
+                                style="width:35%;"
+                                >
+                                <span
+                                    ><img
+                                    class="round"
+                                    src="~assets/img/portrait/small/avatar-s-5.jpg"
+                                    alt="avatar"
+                                    height="40"
+                                    width="40"
+                                /></span>
+                                <div class="d-flex justify-content-end align-items-center">
+                                 <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                </div>
+                                </nuxt-link>
+                            </li>
+                            <li
+                              class="list-group-item d-flex justify-content-between align-items-center"
+                            >
+                            <nuxt-link
+                                class="d-flex justify-content-between"
+                                to="#"
+                                data-toggle="dropdown"
+                                style="width:35%;"
+                                >
+                                <span
+                                    ><img
+                                    class="round"
+                                    src="~assets/img/portrait/small/avatar-s-9.jpg"
+                                    alt="avatar"
+                                    height="40"
+                                    width="40"
+                                /></span>
+                                <div class="d-flex justify-content-end align-items-center">
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                </div>
+                                </nuxt-link>
+                            </li>
+                            <li
+                              class="list-group-item d-flex justify-content-between align-items-center"
+                            >
+                            <nuxt-link
+                                class="d-flex justify-content-between"
+                                to="#"
+                                data-toggle="dropdown"
+                                style="width:35%;"
+                                >
+                                <span
+                                    ><img
+                                    class="round"
+                                    src="~assets/img/portrait/small/avatar-s-5.jpg"
+                                    alt="avatar"
+                                    height="40"
+                                    width="40"
+                                /></span>
+                                <div class="d-flex justify-content-end align-items-center">
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                </div>
+                                </nuxt-link>
+                            </li>
+                            <li
+                              class="list-group-item d-flex justify-content-between align-items-center"
+                            >
+                            <nuxt-link
+                                class="d-flex justify-content-between"
+                                to="#"
+                                data-toggle="dropdown"
+                                style="width:35%;"
+                                >
+                                <span
+                                    ><img
+                                    class="round"
+                                    src="~assets/img/portrait/small/avatar-s-6.jpg"
+                                    alt="avatar"
+                                    height="40"
+                                    width="40"
+                                /></span>
+                                <div class="d-flex justify-content-end align-items-center">
+                                 <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                  <ion-icon name="star-sharp"></ion-icon>
+                                </div>
+                                </nuxt-link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+      <!-- END: Content-->
+
+      <div class="sidenav-overlay"></div>
+      <div class="drag-target"></div>
+
+      <Footer></Footer>
+    </body>
+  </div>
+</template>
+<script>
+import Header from '~/components/nurse/header'
+import Footer from '~/components/nurse/footer'
+import Sidebar from '~/components/nurse/sidebar'
+
+export default {
+  name: 'Message',
+  components: {
+    Header,
+    Footer,
+    Sidebar
+  }
+}
+</script>
