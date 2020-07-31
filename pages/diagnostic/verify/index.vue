ce31518067cb0cedaa8b3812bf3968022aa5c605
OctaconDeveloper <kaakadev@gmail.com>
2020-07-31 12:47:24 +0100

    add diagnostic boiler

--- pages/diagnostic/verify/index.vue
+++ pages/diagnostic/verify/index.vue
@@ -1,15 +1,12 @@
 <template>
   <!-- BEGIN: Content-->
-  <div
-    class="app-content content justify-content-center"
-    style="margin-left:0;"
-  >
+  <div class="app-content content">
     <div class="content-overlay"></div>
     <div class="header-navbar-shadow"></div>
     <div class="content-wrapper">
       <div class="content-header row"></div>
       <div class="content-body">
-        <section class="row flexbox-container justify-content-center">
+        <section class="row flexbox-container">
           <div class="col-xl-8 col-11 d-flex justify-content-center">
             <div class="card rounded-0 mb-0">
               <div class="row m-0">
@@ -20,10 +17,12 @@
                   <img
                     src="~assets/img/logo/drcallaway-logo.png"
                     class="mb-2 mt-1"
-                    width="100px"
-                    height="90px"
+                    width="45px"
+                    height="40px"
                   />
-                  <!-- <h2 class="brand-text mb-2 mt-1" style="align-self: center;">Drcallaway</h2> -->
+                  <h2 class="brand-text mb-2 mt-1" style="align-self: center;">
+                    Drcallaway
+                  </h2>
                 </div>
                 <div class="col-lg-12 col-12 p-0">
                   <div class="card rounded-0 mb-0 px-2">
@@ -33,46 +32,19 @@
                                             </div>
                                         </div> -->
                     <div class="card-title">
-                      <h3 class="text-center"><u>Drcallaway</u></h3>
+                      <h3 class="text-center"><u>Verify Your Account</u></h3>
                     </div>
                     <p class=" text-center">
-                      Welcome to a world where we literally treat health as
-                      wealth.
+                      Kindly verify your email address by clicking the link sent
+                      to your mailbox.
                     </p>
-                    <ion-icon
-                      name="chevron-down-outline"
-                      style="align-self:center;text-alifgn:center"
-                    ></ion-icon>
                     <div class="card-content">
                       <div class="card-body pt-1 d-flex justify-content-center">
                         <nuxt-link
-                          to="/patients/signup"
+                          to="/diagnostic/dashboard"
                           style="text-align: center;"
-                          >Patient |
-                        </nuxt-link>
-                        <nuxt-link
-                          to="/pharmacy/dashboard"
-                          style="text-align: center;"
-                          >Pharmacy |
-                        </nuxt-link>
-                        <nuxt-link
-                          to="/doctor/signup"
-                          style="text-align: center;"
+                          >Click here to continue</nuxt-link
                         >
-                          Doctor |
-                        </nuxt-link>
-                        <nuxt-link
-                          to="/ambulance/signup"
-                           style="text-align: center;"
-                        >
-                          Ambulance |
-                        </nuxt-link>
-                        <nuxt-link
-                          to="/diagnostic/signup"
-                          style="text-align: center;"
-                        >
-                          Diagnostic
-                        </nuxt-link>
                       </div>
                     </div>
                     <div class="login-footer"></div>
@@ -89,6 +61,6 @@
 <!-- END: Content-->
 <script>
 export default {
-  name: 'Home'
+  name: 'Verify'
 }
 </script>
