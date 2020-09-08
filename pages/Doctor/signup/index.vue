9ca164ac2b8c5c6cc9ac40c6b66b0f469686a45c
Oluwatosin Ikudayisi <oluwatosin.ikudayisi@cashdrive.co>
2020-09-08 01:43:44 +0100

    add file upload to document sign up

--- pages/Doctor/signup/index.vue
+++ pages/Doctor/signup/index.vue
@@ -6,6 +6,11 @@
       <div class="content-wrapper">
         <div class="content-header row"></div>
         <div class="content-body">
+          <section>
+              <center>
+                      <img src="~assets/img/logo/drapp.png" class="mb-2 mt-1" width="250px" height="80px">
+              </center>
+          </section>
           <section class="row flexbox-container justify-content-center">
             <div class="col-xl-6 col-12 d-flex justify-content-center">
               <div class="card bg-authentication rounded-0 mb-0">
@@ -135,7 +140,7 @@
                                       type="text"
                                       class="form-control"
                                       placeholder=""
-                                      v-model="form.phone"
+                                      v-model="form.mobile_phone_number"
                                       :class="{'is-invalid': errors.mobile_phone_number}"
                                       style="border-radius:40px;"
                                     />
@@ -259,7 +264,25 @@
                                     </div>
                                   </fieldset>
                                 </div>
-                                
+                                <div class="col-xl-4 col-md-6 col-12 mb-0">
+                                  <fieldset class="form-group">
+                                    <label for="basicInput"
+                                      >Year of Graduation:</label
+                                    >
+                                    <input
+                                      id="basicInput"
+                                      type="file"
+                                      class="form-control"
+                                      placeholder=""
+                                      @change="onImageChange"
+                                      :class="{'is-invalid': errors.medical_certificate}"
+                                      style="border-radius:40px;"
+                                    />
+                                    <div class="invalid-feedback" v-if="errors.medical_certificate">
+                                        {{errors.medical_certificate[0]}}
+                                    </div>
+                                  </fieldset>
+                                </div>
                                 
                               </div>
                               <p class="px-0">
@@ -294,26 +317,6 @@
                                     />
                                     <div class="invalid-feedback" v-if="errors.account_number">
                                         {{errors.account_number[0]}}
-                                    </div>
-                                  </fieldset>
-                                </div>
-                                <div class="col-xl-6 col-md-6 col-12 mb-0">
-                                  <fieldset class="form-group">
-                                    <label for="basicInput">Account Type:</label>
-                                    <select
-                                      id="basicInput"
-                                      type="text"
-                                      class="form-control"
-                                      v-model="form.account_type"
-                                      :class="{'is-invalid': errors.account_type}"
-                                      style="border-radius:40px;"
-                                    >
-                                    <option value="Savings">Savings</option>
-                                    <option value="Current">Current</option>
-                                    
-                                    </select>
-                                    <div class="invalid-feedback" v-if="errors.account_type">
-                                        {{errors.account_type[0]}}
                                     </div>
                                   </fieldset>
                                 </div>
@@ -370,6 +373,7 @@ export default {
           last_name : "",
           middle_name : "",
           email : "",
+          medical_certificate:"",
           password : "",
           password_confirmation : "",
           gender : "",
@@ -380,7 +384,6 @@ export default {
           specialty_code: "",
           account_name: "",
           account_number: "",
-          account_type: "",
           bank_code: "",
           school_attended: ""
        },
@@ -410,6 +413,10 @@ export default {
             this.disable = !this.disable
         })
     },
+    onImageChange(e){
+      console.log(e.target.files[0]);
+      this.form.medical_certificate = e.target.files[0];
+    },
     validateInput(){
         
         return  this.form.first_name &&
@@ -420,10 +427,15 @@ export default {
          this.form.mcrn && 
          this.form.bank_code && 
          this.form.account_name && 
-         this.form.account_number && 
-         this.form.account_type && 
+         this.form.account_number &&
          this.form.specialty_code || false
         
+    },
+    validateImage(){
+      return this.form.medical_certificate.type == "image/png" || this.form.medical_certificate.type == "image/jpg" || this.form.medical_certificate.type == "image/jpeg" ? true : false
+    },
+    validateImageSize(){
+      return this.form.medical_certificate.size <= 20000 || false
     },
     checkPassword(){
       return this.form.password.length > 5 || false
@@ -434,29 +446,64 @@ export default {
     async submit(){
       
       if(!this.validateInput()){
-        alert("please fill in the required fields");
+        this.$noty.error("please fill in the required fields");
         return false;
       }
       
       if(!this.checkPassword()){
-        alert("password must be atleast than six(6) characters long");
+        this.$noty.error("password must be atleast than six(6) characters long");
         return false;
       }
 
       if(!this.matchPassword()){
-        alert("passwords do not match");
+        this.$noty.error("passwords do not match");
+        return false;
+      }
+      if(!this.form.medical_certificate){
+        this.$noty.error("Please upload your medical certificate.");
+        return false;
+      }
+      if(!this.validateImage()){
+        this.$noty.error("Please upload the right document.");
         return false;
       }
 
+      // if(!this.validateImageSize()){
+      //   this.$noty.error("Please file size should not exceed 2MB.");
+      //   return false;
+      // }
+      
+      let formData = new FormData();
+      formData.append('first_name', this.form.first_name);
+      formData.append('last_name', this.form.last_name);
+      formData.append('middle_name', this.form.middle_name);
+      formData.append('email', this.form.email);
+      formData.append('medical_certificate', this.form.medical_certificate);
+      formData.append('password', this.form.password);
+      formData.append('password_confirmation', this.form.password_confirmation);
+      formData.append('gender', this.form.gender);
+      formData.append('mobile_phone_number', this.form.mobile_phone_number);
+      formData.append('user_role_code', this.form.user_role_code);
+      formData.append('mcrn', this.form.mcrn);
+      formData.append('year_of_graduation', this.form.year_of_graduation);
+      formData.append('specialty_code', this.form.specialty_code);
+      formData.append('account_name', this.form.gender);
+      formData.append('account_number', this.form.account_number);
+      formData.append('bank_code', this.form.bank_code);
+      formData.append('school_attended', this.form.school_attended);
+      
+      const config = {
+          headers: { 'content-type': 'multipart/form-data' }
+      }
       this.disable = !this.disable
-        await this.$axios.post('doctors/signup', this.form
+        await this.$axios.post('doctors/signup', formData, config
         ).then(response => {
             this.$router.push({
               path:'/auth/verify'
             }) 
         })
         .catch(error => {
-            console.log(error.response)
+            // console.log(error.response)
             this.disable = !this.disable
         })
         
