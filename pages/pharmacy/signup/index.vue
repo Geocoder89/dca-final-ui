57b924532aa10a8067c57d59e176091375edc7e4
Oluwatosin Ikudayisi <oluwatosin.ikudayisi@cashdrive.co>
2020-09-24 00:13:18 +0100

    set up parmacy sign up

--- pages/pharmacy/signup/index.vue
+++ pages/pharmacy/signup/index.vue
@@ -25,7 +25,7 @@
                           <div class="col-12 pl-0">
                             <form action="">
                               <div class="row">
-                                <div class="col-xl-4 col-md-6 col-12 mb-0">
+                                <div class="col-xl-6 col-md-6 col-12 mb-0">
                                   <fieldset class="form-group">
                                     <label for="basicInput">Pharmacy Name:</label>
                                     <input
@@ -33,23 +33,13 @@
                                       type="text"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="form.name"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
                                 </div>
-                                <div class="col-xl-4 col-md-6 col-12 mb-0">
-                                  <fieldset class="form-group">
-                                    <label for="basicInput">Pharmacy Address:</label>
-                                    <input
-                                      id="basicInput"
-                                      type="text"
-                                      class="form-control"
-                                      placeholder=""
-                                      style="border-radius:40px;"
-                                    />
-                                  </fieldset>
-                                </div>
-                                <div class="col-xl-4 col-md-6 col-12 mb-0">
+                                
+                                <div class="col-xl-6 col-md-6 col-12 mb-0">
                                   <fieldset class="form-group">
                                     <label for="basicInput">Pharmacy License No:</label>
                                     <input
@@ -57,6 +47,20 @@
                                       type="text"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="form.license_number"
+                                      style="border-radius:40px;"
+                                    />
+                                  </fieldset>
+                                </div>
+                                <div class="col-xl-12 col-md-12 col-12 mb-0">
+                                  <fieldset class="form-group">
+                                    <label for="basicInput">Pharmacy Address:</label>
+                                    <input
+                                      id="basicInput"
+                                      type="text"
+                                      class="form-control"
+                                      placeholder=""
+                                      v-model="form.address"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
@@ -69,6 +73,7 @@
                                       type="text"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="form.email"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
@@ -81,10 +86,57 @@
                                       type="text"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="form.phone"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
                                 </div>
+                                
+                                <div class="col-xl-6 col-md-6 col-12 mb-0">
+                                  <fieldset class="form-group">
+                                    <label for="basicInput">Partner Type:</label>
+                                    <select
+                                      id="basicInput"
+                                      type="text"
+                                      class="form-control"
+                                      v-model="form.partner_type"
+                                      style="border-radius:40px;"
+                                    >
+                                    <option value="" >Select Partner</option>
+                                    <option v-for="(p,i) in partner_types" :key="i" :value="p">{{p}}</option>
+                                    </select>
+                                  </fieldset>
+                                </div>
+                                <div class="col-xl-12 col-md-12 col-12 mb-0">
+                                  <fieldset class="form-group">
+                                    <label for="basicInput">Description of Pharmacy:</label>
+                                    <textarea
+                                      id="basicInput"
+                                      v-model="form.description"
+                                      style="resize:none;border-radius:40px;"
+                                      class="form-control">
+                                    </textarea>
+                                  </fieldset>
+                                </div>
+                              </div>
+                              <p class="px-0">
+                                    Representatives #1
+                            </p>
+                            <div class="row">
+                                <div class="col-xl-6 col-md-6 col-12 mb-0">
+                                  <fieldset class="form-group">
+                                    <label for="basicInput">Email:</label>
+                                    <input
+                                      id="basicInput"
+                                      type="text"
+                                      class="form-control"
+                                      placeholder=""
+                                      v-model="second_user.email"
+                                      style="border-radius:40px;"
+                                    />
+                                  </fieldset>
+                                </div>
+                                
                                 <div class="col-xl-6 col-md-6 col-12 mb-0">
                                   <fieldset class="form-group">
                                     <label for="basicInput"
@@ -95,6 +147,7 @@
                                       type="password"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="second_user.password"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
@@ -109,87 +162,72 @@
                                       type="password"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="second_user.password_confirmation"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
                                 </div>
-                                <div class="col-xl-12 col-md-12 col-12 mb-0">
-                                  <fieldset class="form-group">
-                                    <label for="basicInput">Description of Pharmacy:</label>
-                                    <textarea
-                                      id="basicInput"
-                                      style="resize:none;border-radius:40px;"
-                                      class="form-control">
-                                    </textarea>
-                                  </fieldset>
-                                </div>
-                              </div>
-                              <p class="px-0">
-                                    Bank Details.
+                                
+                                
+                            </div>    
+                            <p class="px-0">
+                                    Representatives #2
                             </p>
                             <div class="row">
+                                
                                 <div class="col-xl-6 col-md-6 col-12 mb-0">
                                   <fieldset class="form-group">
-                                    <label for="basicInput">Account Name:</label>
+                                    <label for="basicInput">Email:</label>
                                     <input
                                       id="basicInput"
                                       type="text"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="first_user.email"
+                                      style="border-radius:40px;"
+                                    />
+                                  </fieldset>
+                                </div>
+                                
+                                <div class="col-xl-6 col-md-6 col-12 mb-0">
+                                  <fieldset class="form-group">
+                                    <label for="basicInput"
+                                      >Password:</label
+                                    >
+                                    <input
+                                      id="basicInput"
+                                      type="password"
+                                      class="form-control"
+                                      placeholder=""
+                                      v-model="first_user.password"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
                                 </div>
                                 <div class="col-xl-6 col-md-6 col-12 mb-0">
                                   <fieldset class="form-group">
-                                    <label for="basicInput">Account No:</label>
+                                    <label for="basicInput"
+                                      >Password Confirmation:</label
+                                    >
                                     <input
                                       id="basicInput"
-                                      type="text"
+                                      type="password"
                                       class="form-control"
                                       placeholder=""
+                                      v-model="first_user.password_confirmation"
                                       style="border-radius:40px;"
                                     />
                                   </fieldset>
                                 </div>
-                                <div class="col-xl-6 col-md-6 col-12 mb-0">
-                                  <fieldset class="form-group">
-                                    <label for="basicInput">Account Type:</label>
-                                    <select
-                                      id="basicInput"
-                                      type="text"
-                                      class="form-control"
-                                      style="border-radius:40px;"
-                                    >
-                                    <option value="">Field one</option>
-                                    <option value="">Field one</option>
-                                    <option value="">Field one</option>
-                                    </select>
-                                  </fieldset>
-                                </div>
-                                <div class="col-xl-6 col-md-6 col-12 mb-0">
-                                  <fieldset class="form-group">
-                                    <label for="basicInput">Choose Your Bank:</label>
-                                    <select
-                                      id="basicInput"
-                                      type="text"
-                                      class="form-control"
-                                      style="border-radius:40px;"
-                                    >
-                                    <option value="">Field one</option>
-                                    <option value="">Field one</option>
-                                    <option value="">Field one</option>
-                                    </select>
-                                  </fieldset>
-                                </div>
+                                
                             </div>        
                               <div class="col-12 pl-0">
                                 <p>
                                   Already have an account?
                                   <nuxt-link to="/pharmacy/login">login</nuxt-link>
                                 </p>
-                                <nuxt-link to="/pharmacy/verify" style="border-radius:40px;" class="btn btn-primary">
-                                  Register</nuxt-link
+                                <button style="border-radius:40px;" :disabled="this.disable" class="btn btn-primary" @click.prevent="submit">
+                                  Register</button
                                 >
                               </div>
                             </form>
@@ -210,6 +248,139 @@
 </template>
 <script>
 export default {
-  name: 'Signup'
+  name: 'Signup',
+  data(){
+    return {
+       form:{
+          name : "",
+          address : "",
+          license_number : "",
+          email : "",
+          phone:"",
+          description : "",
+          partner_type : "",
+          members:[]
+       },
+       first_user:{
+          first_name : "",
+          last_name : "",
+          email : "",
+          password : "",
+          password_confirmation : ""
+       },
+        second_user:{
+          first_name : "",
+          last_name : "",
+          email : "",
+          password : "",
+          password_confirmation : ""
+        },
+       partner_types:["DIAGNOSTIC","AMBULANCE","HOSPITAL","PHARMACY"],
+       disable: false
+    }
+  },
+  methods:{
+    validateInput(){
+        
+      return  this.form.name &&
+      this.form.address && this.form.license_number && 
+      this.form.email && 
+      this.form.phone && 
+      this.form.description && 
+      this.form.partner_type &&
+      this.first_user.email &&
+      this.first_user.password &&
+      this.first_user.password_confirmation &&
+      this.second_user.email &&
+      this.second_user.password &&
+      this.second_user.password_confirmation || false
+        
+    },
+    validateImage(){
+      return this.form.medical_certificate.type == "image/png" || this.form.medical_certificate.type == "image/jpg" || this.form.medical_certificate.type == "image/jpeg" ? true : false
+    },
+    validateImageSize(){
+      return this.form.medical_certificate.size <= 20000 || false
+    },
+    checkPassword(password){
+      return password > 5 || false
+    },
+    matchPassword(password, confirm_password){
+      return password === confirm_password || false
+    },
+    async submit(){
+      
+      if(!this.validateInput()){
+        this.$noty.error("please fill in the required fields");
+        return false;
+      }
+      
+
+      if(!this.checkPassword(this.first_user.password)){
+        this.$noty.error("representative password must be atleast than six(6) characters long");
+        return false;
+      }
+
+      if(!this.checkPassword(this.second_user.password)){
+        this.$noty.error("representative password must be atleast than six(6) characters long");
+        return false;
+      }
+
+      if(!this.matchPassword(this.first_user.password, this.first_user.password_confirmation)){
+        this.$noty.error("representative passwords do not match");
+        return false;
+      }
+
+      if(!this.matchPassword(this.second_user.password, this.second_user.password_confirmation)){
+        this.$noty.error("representative passwords do not match");
+        return false;
+      }
+      
+      this.form.members.push(this.first_user)
+      this.form.members.push(this.second_user)
+      
+      // let formData = new FormData();
+      // formData.append('name', this.form.name);
+      // formData.append('email', this.form.email);
+      // formData.append('address', this.form.address);
+      // formData.append('license_number', this.form.license_number);
+      // formData.append('partner_type', this.form.partner_type);
+      // formData.append('phone_number', this.form.phone);
+      // formData.append('description', this.form.description);
+      // formData.append('members', []);
+      
+      let datar = {
+          name : this.form.name,
+          email : this.form.email,
+          address : this.form.address,
+          license_number : this.form.license_number,
+          partner_type : this.form.partner_type,
+          phone_number : this.form.phone,
+          description : this.form.description,
+          members : this.form.members
+        }
+      
+      const config = {
+          headers: { 'content-type': 'multipart/form-data' }
+      }
+    
+      this.disable = !this.disable
+        await this.$axios.post('partners/signup', datar , {config}
+        ).then(response => {
+            this.$router.push({
+              path:'/auth/verify'
+            }) 
+        })
+        .catch(error => {
+            
+            this.disable = !this.disable
+        })
+      
+    }
+  },
+  mounted(){
+      
+  },
+  middleware:['guest']
 }
 </script>
